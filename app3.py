45d2e8608f82bcae61d10dc29dc08161e84647af
Patrick James Pangilinan <patrickjpangilinan@protonmail.com>
2025-05-23 14:21:04 +0800

    Update app3.py

--- app3.py
+++ app3.py
@@ -6,9 +6,7 @@ import pages as pg
 
 st.set_page_config(page_title="Taal Lake Water Quality Dashboard", layout="wide", page_icon="lawatch.svg", initial_sidebar_state="collapsed")
 
-pages = ["Home", "Dashboard", "Recommendations"]
-parent_dir = os.path.dirname(os.path.abspath(__file__))
-logo_path = os.path.join(parent_dir, "lawatch.svg")
+pages = ["Dashboard", "Recommendations"]
 
 styles = {
     "nav": {
@@ -19,9 +17,6 @@ styles = {
         "justify-content": "space-between",
         "margin-bottom": "0",
     },
-    "img": {
-        "height": "4.5rem",
-    },
     "span": {
         "color": "white",
         "font-size": "1.2rem",
@@ -29,22 +24,16 @@ styles = {
         "font-weight": "bold",
     },
     "active": {
-        "color": "#f8cc63",  # gold-ish active color
+        "color": "#f8cc63", 
         "text-decoration": "underline",
         "font-weight": "bold",
     }
 }
 page = st_navbar(
     pages,
-    logo_path=logo_path,
     styles=styles,
 )
 
-functions = {
-    "Home": pg.show_home,
-    "Dashboard": pg.show_dashboard,
-    "Recommendations": pg.show_rec,
-}
 st.markdown(
     """
     <style>
@@ -83,8 +72,3 @@ st.markdown(
     """,
     unsafe_allow_html=True
 )
-
-
-go_to = functions.get(page)
-if go_to:
-    go_to()
