ef63d0d6d50aa9badb36bc194a7421ff9e6af996
Patrick James Pangilinan <patrickjpangilinan@protonmail.com>
2025-05-23 20:35:24 +0800

    Update app3.py

--- app3.py
+++ app3.py
@@ -6,7 +6,7 @@ import pages as pg
 
 st.set_page_config(page_title="Taal Lake Water Quality Dashboard", layout="wide", page_icon="lawatch.svg", initial_sidebar_state="collapsed")
 
-pages = ["Dashboard", "Recommendations"]
+pages = ["Dashboard", "Recommendations", "Lol"]
 
 styles = {
     "nav": {
@@ -35,6 +35,7 @@ styles = {
 page = st_navbar(
     pages,
     logo_path="lawatch.svg",
+    styles=styles
 )
 
 functions = {
