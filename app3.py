277211e17ac2a29999c3f4ac39b8922d9394c60f
Patrick James Pangilinan <patrickjpangilinan@protonmail.com>
2025-05-23 20:36:48 +0800

    Update app3.py

--- app3.py
+++ app3.py
@@ -43,44 +43,7 @@ functions = {
     "Dashboard": pg.show_dashboard,
     "Recommendations": pg.show_rec,
 }
-st.markdown(
-    """
-    <style>
-    .block-container {
-        padding-top: 1rem !important;
-    }
-    
-    .nav-list li:nth-child(1) span {
-        color: orange !important;
-        font-style: italic !important;
-    }
 
-    .nav-bottom-line {
-        position: fixed;
-        top: 6rem; /* Adjust to the height of your navbar */
-        left: 0;
-        right: 0;
-        height: 0.5rem;
-        background-color: #1a4723;
-        z-index: 9999;
-    }
-
-    /* Optional: remove top border if unwanted */
-    body {
-        margin: 0;
-        padding: 0;
-    }
-
-    header[data-testid="stHeader"] {
-        margin: 0;
-        padding: 0;
-        border-top: none;
-    }
-    </style>
-    <div class="nav-bottom-line"></div>
-    """,
-    unsafe_allow_html=True
-)
 
 
 go_to = functions.get(page)
